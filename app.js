9eeb53d38a12aad598bc9e81a41d1adc3d8114f6
Sashankr <sashankr97@gmail.com>
2023-06-08 14:27:05 +0530

    added body parser in express app

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-const http = require('http');
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
+
+app.post("/addToDo", (req, res) => {
+  console.log(req.body);
+});
+
+app.set("view engine", "ejs");
+
+app.listen(3000, () => console.log("Server Running"));
